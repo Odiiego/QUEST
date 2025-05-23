7184ad83663e4b06d8887dafc7a8e2de0eb29edc
Diego <odiegodev@gmail.com>
2025-05-22 22:49:29 -0300

    chore: inicia o projeto react

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import Recover from './pages/Recover/Recover';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/recover" element={<Recover />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
